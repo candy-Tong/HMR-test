2159c69bd29957f00ee5b8ddbd671cc55a879907
candy-Tong <563378816@qq.com>
2023-01-03 20:25:30 +0800

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ export const render = () => {
   el.innerHTML = `
     <h1>Project: ts-file-test</h1>
     <h2>File: accept.ts</h2>
-    <p>accept test</p>
+    <p>accept test2</p>
   `;
 };
 render();
 
+// 如果没有下面这一段，修改代码后，整个页面会刷新
 if (import.meta.hot) {
   // 调用的时候，调用的是老的模块的 accept 回调
   import.meta.hot.accept((mod) => {
