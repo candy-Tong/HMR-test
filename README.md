43a14f3c12b47190358bd5dea9ec42bade291936
candy-Tong <563378816@qq.com>
2023-01-03 20:19:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HMR-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-1bvzkf)
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+export const render = () => {
+  const el = document.querySelector<HTMLDivElement>('#app')!;
+  el.innerHTML = `
+    <h1>Project: ts-file-test</h1>
+    <h2>File: accept.ts</h2>
+    <p>accept test</p>
+  `;
+};
+render();
+
+if (import.meta.hot) {
+  // 调用的时候，调用的是老的模块的 accept 回调
+  import.meta.hot.accept((mod) => {
+    if (mod) {
+      // 老的模块的 accept 回调拿到的是新的模块
+      console.log('mod', mod);
+      console.log('mod.render', mod.render);
+      mod.render();
+    }
+  });
+}
